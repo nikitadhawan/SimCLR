bec2638e15098711371f789e60a607f849c68a9a
Ahmad <ahmad.kaleem@mail.utoronto.ca>
2022-01-22 21:48:14 -0500

    add stl10

--- linsimsiam.py
+++ linsimsiam.py
@@ -531,6 +531,31 @@ def main_worker(gpu, ngpus_per_node, args):
             test_dataset, batch_size=args.batch_size, shuffle=False,
             num_workers=args.workers)
 
+    elif args.dataset == "stl10":
+        transform_train = transforms.Compose([
+            transforms.Resize(224),
+            # transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+
+        transform_test = transforms.Compose([
+            transforms.Resize(224),
+            transforms.ToTensor(),
+        ])
+
+        train_dataset = datasets.STL10(
+            f"/checkpoint/{os.getenv('USER')}/SimCLR/stl10", split='train',
+            download=False,transform=transform_train)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size,
+                                num_workers=0, drop_last=False, shuffle=True)
+        test_dataset = datasets.STL10(f"/checkpoint/{os.getenv('USER')}/SimCLR/stl10", split='test', download=False,
+                                      transform=transform_test)
+        val_loader = DataLoader(test_dataset, batch_size=batch_size,
+                                num_workers=2, drop_last=False, shuffle=False)
+
     elif args.dataset == 'svhn':
 
         transform_svhn = transforms.Compose([
@@ -552,7 +577,7 @@ def main_worker(gpu, ngpus_per_node, args):
             num_workers=args.workers)
 
     else:
-        raise Exception(f"Unknown args.datasetsteal: {args.datasetsteal}.")
+        raise Exception(f"Unknown args.dataset: {args.dataset}.")
     if args.distributed:
         train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
     else:
