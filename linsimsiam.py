ed9a3d1fe75490d002835532e01155e66a423f98
ahmadkaleem123 <ahmad.kaleem@mail.utoronto.ca>
2022-01-23 00:46:34 +0000

    epoch num

--- linsimsiam.py
+++ linsimsiam.py
@@ -54,7 +54,7 @@ parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet50',
                         ' (default: resnet50)')
 parser.add_argument('-j', '--workers', default=20, type=int, metavar='N',
                     help='number of data loading workers (default: 32)')
-parser.add_argument('--epochs', default=30, type=int, metavar='N',
+parser.add_argument('--epochs', default=100, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
